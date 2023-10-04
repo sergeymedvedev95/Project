eda7133562d6bff281b42877d8cd9aeeece64c61
sergeymedvedev95 <145459941+sergeymedvedev95@users.noreply.github.com>
2023-10-04 14:34:03 +0300

    Updated app.py

--- app.py
+++ app.py
@@ -10,13 +10,7 @@ import streamlit as st
 import pandas as pd 
 import plotly_express as px
 
-st.write("""
-### Sergey Medvedev Sprint 6 Project
-""")
-
-st.write("""
-#### Table of data
-""")
+st.header('Sergey Medvedev Sprint 6 Project')
 
 df = pd.read_csv('vehicles_us.csv')
 
@@ -33,36 +27,47 @@ for column in another_column_to_replace:
     print(column)
     df[column] = df[column].fillna('unknown')
     print('missing values in ', column, 'are replaced')
-st.table(df.head())
+
+st.header('Table of Data')
+
+show_table = st.checkbox('Show data table slice')
+if show_table :
+    st.write("""
+    ### Data table slice
+    """)
+    st.table(df.head())
 
 df.insert(0, 'id', range(0, 0 + len(df)))
+grouped_cars = df.groupby(['type'])['model'].nunique().reset_index()
+grouped_cars_sorted = grouped_cars.sort_values(by = 'model',ascending=False)
 
 st.write("""
-### Proportion of different types of vehicles
+### Number of models per vehicle type shown in a descending order
 """)
-grouped_cars = df.groupby('type')['id'].nunique().reset_index()
-st.table(grouped_cars)
+show_table = st.checkbox('Show table')
+if show_table : 
+    st.write("""
+    ### Table of number of different models per type of vehicle
+    """)
+    grouped_cars = df.groupby(['type'])['model'].nunique().reset_index()
+    grouped_cars_sorted = grouped_cars.sort_values(by = 'model',ascending=False)
+    st.table(grouped_cars_sorted)
 
 st.write("""
 ### Histogram of popularity of different types of vehicles
 """)
 
-hist = px.histogram(df, x='type', color = 'type',
-                    labels={
-                     'type' : 'Type'}).update_xaxes(categoryorder = 'total descending')
-st.plotly_chart(hist)
+histogram = px.bar(grouped_cars_sorted, x=grouped_cars_sorted.type, y=grouped_cars_sorted.model)
+histogram.update_layout(title="<b> Popularity of the bodystyle")
+show_histogram_bodystyle = st.checkbox('Show the histogram of popularity of the bodystyle')
+if show_histogram_bodystyle :
+    st.plotly_chart(histogram)
 
-
-st.write("""
-### Pie chart of the representation of different types of vehicles
-""")
-
-pie = px.pie(grouped_cars, values=grouped_cars.id, names=grouped_cars.type)
-st.plotly_chart(pie)
 
 st.write("""
 ### Scatterplot of various models of vehicles against their prices, represented by different types of cars 
 """)
+
 
 # Scatter plot
 fig = px.scatter(df, x='model', y='price', color='type',
@@ -71,7 +76,9 @@ fig = px.scatter(df, x='model', y='price', color='type',
                      'price' : 'Price',
                      'type' : 'Type'
                  })
-st.plotly_chart(fig)
+show_scatter_plot = st.checkbox('Show scatter plot of model of vehicles against price')
+if show_scatter_plot :
+    st.plotly_chart(fig)
 
 st.write("""
 ### Scatterplot of years of vehicles against their price, represented by different conditions of vehicles 
@@ -83,11 +90,12 @@ fig_1 = px.scatter(df, x='model_year', y='price', color='condition',
                      'price' : 'Price',
                      'condition' : 'Condition'
                  })
-st.plotly_chart(fig_1)
-
+show_scatter_plot1 = st.checkbox('Show scatterplot of prices against year of vehicles')
+if show_scatter_plot1 :
+    st.plotly_chart(fig_1)
 
 st.write("""
-## Block with Filtered Data 
+## Block with Filtered Data - Filter on the left side on the screen works only with following two charts
 """)
 
 rest_type = df['type'].unique()
@@ -104,11 +112,15 @@ fig_2 = px.scatter(filtered_type, x='model_year', y='price', color='type',
                      'price' : 'Price',
                      'type' : 'Type'
                  })
-st.plotly_chart(fig_2)
+show_scatter_plot2 = st.checkbox('Show scatterplot of prices against year of vehicles filtered by type of vehicle')
+if show_scatter_plot2:
+    st.plotly_chart(fig_2)
 
 st.write("""
 ### Histogram with filtered data
 """)
-hist_filt = px.histogram(filtered_type, x='condition', color='cylinders').update_xaxes(categoryorder = 'total descending')
-st.plotly_chart(hist_filt)
+hist_filt = px.bar(filtered_type, x=filtered_type.model, y=filtered_type.cylinders, color= 'condition').update_xaxes(categoryorder = 'total descending')
+show_histogram = st.checkbox('Show histogram of different models of vehicles based on the amount of cylinders, differentiated by vehicle condition')
+if show_histogram:
+    st.plotly_chart(hist_filt)
 
