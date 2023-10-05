baa379b19de5312ebfb7209b3946a8c47f4d1c73
sergeymedvedev95 <145459941+sergeymedvedev95@users.noreply.github.com>
2023-10-05 12:40:55 +0300

    Updated app.py

--- app.py
+++ app.py
@@ -60,14 +60,11 @@ st.write("""
 histogram = px.bar(grouped_cars_sorted, x=grouped_cars_sorted.type, y=grouped_cars_sorted.model)
 histogram.update_layout(title="<b> Popularity of the bodystyle")
 show_histogram_bodystyle = st.checkbox('Show the histogram of popularity of the bodystyle')
-if show_histogram_bodystyle :
-    st.plotly_chart(histogram)
-
+st.plotly_chart(histogram)
 
 st.write("""
 ### Scatterplot of various models of vehicles against their prices, represented by different types of cars 
 """)
-
 
 # Scatter plot
 fig = px.scatter(df, x='model', y='price', color='type',
@@ -76,9 +73,7 @@ fig = px.scatter(df, x='model', y='price', color='type',
                      'price' : 'Price',
                      'type' : 'Type'
                  })
-show_scatter_plot = st.checkbox('Show scatter plot of model of vehicles against price')
-if show_scatter_plot :
-    st.plotly_chart(fig)
+st.plotly_chart(fig)
 
 st.write("""
 ### Scatterplot of years of vehicles against their price, represented by different conditions of vehicles 
@@ -90,37 +85,53 @@ fig_1 = px.scatter(df, x='model_year', y='price', color='condition',
                      'price' : 'Price',
                      'condition' : 'Condition'
                  })
-show_scatter_plot1 = st.checkbox('Show scatterplot of prices against year of vehicles')
-if show_scatter_plot1 :
-    st.plotly_chart(fig_1)
+st.plotly_chart(fig_1)
 
 st.write("""
 ## Block with Filtered Data - Filter on the left side on the screen works only with following two charts
 """)
 
-rest_type = df['type'].unique()
-make_choice = st.sidebar.selectbox('Select type of vehicle:', rest_type)
-filtered_type = df[df.type==make_choice]
+price_range = st.slider(
+     "What is your price range?",
+     value=(0, 375000))
+
+actual_range=list(range(price_range[0],price_range[1]+1))
+
+option = st.selectbox(
+    'How many cylinders do you prefer',
+    ('6','8', '10', '12'))
+st.write('You selected:', option)
+
+
+if option == '6':
+    filtered_data=df[df.price.isin(actual_range)]
+    filtered_data=filtered_data[df.cylinders == 6]
+elif option == '8':
+    filtered_data=df[df.price.isin(actual_range)]
+    filtered_data=filtered_data[df.cylinders == 8]
+elif option == '10':
+    filtered_data=df[df.price.isin(actual_range)]
+    filtered_data=filtered_data[df.cylinders == 10]
+elif option == '12':
+    filtered_data=df[df.price.isin(actual_range)]
+    filtered_data=filtered_data[df.cylinders == 12]
+else:
+    filtered_data=df[df.price.isin(actual_range)]
 
 st.write("""
 ### Scatterplot with filtered data
 """)
 
-fig_2 = px.scatter(filtered_type, x='model_year', y='price', color='type',
+fig_2 = px.scatter(filtered_data, x='model_year', y='price', color='type',
                   labels={
                      'model_year' : 'Model',
                      'price' : 'Price',
                      'type' : 'Type'
                  })
-show_scatter_plot2 = st.checkbox('Show scatterplot of prices against year of vehicles filtered by type of vehicle')
-if show_scatter_plot2:
-    st.plotly_chart(fig_2)
+st.plotly_chart(fig_2)
 
 st.write("""
 ### Histogram with filtered data
 """)
-hist_filt = px.bar(filtered_type, x=filtered_type.model, y=filtered_type.cylinders, color= 'condition').update_xaxes(categoryorder = 'total descending')
-show_histogram = st.checkbox('Show histogram of different models of vehicles based on the amount of cylinders, differentiated by vehicle condition')
-if show_histogram:
-    st.plotly_chart(hist_filt)
-
+hist_filt = px.bar(filtered_data, x=filtered_data.price, y=filtered_data.type, color= 'condition').update_xaxes(categoryorder = 'total descending')
+st.plotly_chart(hist_filt)
